9dbae03f719e8a56bdc1c895c8d8fc34f8675f6f
August Skare <post@augustskare.no>
2015-10-21 19:35:37 +0200

    added grunt++ tasks for minifying html and inlining css

---
+++ tasks/options/htmlmin.js
@@ -0,0 +1,9 @@
+module.exports = {
+  dist: {
+    options: {
+      removeComments: true,
+      collapseWhitespace: true
+    },
+    files: { 'build/index.html' : 'build/index.html' }
+  },
+}
---
+++ tasks/options/inline.js
@@ -0,0 +1,9 @@
+module.exports = {
+  dist: {
+    options:{
+      cssmin: true
+    },
+    src: 'public/index.html',
+    dest: 'build/index.html'
+  }
+}
