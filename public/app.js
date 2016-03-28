8e5cc224c65fbfaa069acecc29aee79702c2a62b
August Skare <post@augustskare.no>
2016-03-28 22:56:45 +0200

    changed arrow function to es5 function

--- public/app.js
+++ public/app.js
@@ -5,7 +5,7 @@
   h=d.documentElement,t=setTimeout(function(){h.className=h.className.replace(/\bwf-loading\b/g,"")+" wf-inactive";},config.scriptTimeout),tk=d.createElement("script"),f=false,s=d.getElementsByTagName("script")[0],a;h.className+=" wf-loading";tk.src='https://use.typekit.net/'+config.kitId+'.js';tk.async=true;tk.onload=tk.onreadystatechange=function(){a=this.readyState;if(f||a&&a!="complete"&&a!="loaded")return;f=true;clearTimeout(t);try{Typekit.load(config)}catch(e){}};s.parentNode.insertBefore(tk,s)
 
   if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.register('/sw.js').then( (reg) => {
+    navigator.serviceWorker.register('/sw.js').then(function(reg) {
       if (!navigator.serviceWorker.controller) { return; }
 
       if (reg.waiting) {
@@ -21,9 +21,8 @@
       reg.addEventListener('updatefound', function() {
         _trackInstalling(reg.installing);
       });
-
     });
-
+    
     var refreshing;
     navigator.serviceWorker.addEventListener('controllerchange', function() {
       if (refreshing) { return; }
