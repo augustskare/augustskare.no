8863ced02a4bc6b79a934510505dde0e3b6b1fd7
August Skare <post@augustskare.no>
2019-12-31 16:23:59 +0100

    Inline css from file

--- .eleventy.js
+++ .eleventy.js
@@ -1,9 +1,13 @@
 const pluginRss = require("@11ty/eleventy-plugin-rss");
+const CleanCSS = require("clean-css");
 
 module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy("src/assets");
   eleventyConfig.addFilter("htmlDateString", dateObj => {
     return DateTime.fromJSDate(dateObj, { zone: "utc" }).toFormat("yyyy-LL-dd");
+  });
+  eleventyConfig.addFilter("cssmin", css => {
+    return new CleanCSS({}).minify(css).styles;
   });
 
   eleventyConfig.addCollection("notes", collection => {
