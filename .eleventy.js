27190818e5f858dd31a9b83914def73129888b29
August Skare <post@augustskare.no>
2023-01-11 22:42:24 +0100

    Finish it..

--- .eleventy.js
+++ .eleventy.js
@@ -1,29 +1,24 @@
-const pluginRss = require("@11ty/eleventy-plugin-rss");
 const CleanCSS = require("clean-css");
 const { default: feedbinStars } = require("eleventy-plugin-feedbin-stars");
 
-module.exports = function(eleventyConfig) {
+module.exports = function (eleventyConfig) {
   eleventyConfig.addPlugin(feedbinStars, {
     feed_id: "5Euw2uq5T9oUL7tUbs0_2A",
   });
   eleventyConfig.addPassthroughCopy("src/assets");
-  eleventyConfig.addFilter("htmlDateString", dateObj => {
+  eleventyConfig.addFilter("htmlDateString", (dateObj) => {
     return DateTime.fromJSDate(dateObj, { zone: "utc" }).toFormat("yyyy-LL-dd");
   });
-  eleventyConfig.addFilter("cssmin", css => {
+  eleventyConfig.addFilter("cssmin", (css) => {
     return new CleanCSS({}).minify(css).styles;
   });
-  eleventyConfig.addFilter("host", url => {
-    return new URL(url).host;
-  });
+  eleventyConfig.addFilter("host", (url) => {
+    if (url) {
+      return new URL(url).host;
+    }
 
-  eleventyConfig.addCollection("notes", collection => {
-    return collection
-      .getAll()
-      .filter(({ inputPath }) => inputPath.includes("notes/"));
+    return "Invalid url";
   });
-
-  eleventyConfig.addPlugin(pluginRss);
 
   return {
     templateFormats: ["md", "njk", "html", "liquid"],
